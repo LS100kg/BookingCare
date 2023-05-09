374d5b2dfa1e101978d10cdbfbc9cd3c2d9b3ed7
LS100kg <taquangson5011@gmail.com>
2023-05-09 16:17:57 +0700

    090523

---
+++ src/services/CRUDService.js
@@ -0,0 +1,57 @@
+import bcrypt from "bcryptjs";
+import db from "../models";
+
+const salt = bcrypt.genSaltSync(10);
+let createNewUser = async (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+            await db.User.create({
+                email: data.email,
+                password: hashPasswordFromBcrypt,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                address: data.address,
+                phoneNumber: data.phoneNumber,
+                gender: data.gender === '1' ? true : false,
+                roleID: data.roleID,
+            })
+
+            resolve('Create success');
+
+        } catch (e) {
+            reject(e);
+        }
+    })
+
+}
+
+let hashUserPassword = (password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPassword = await bcrypt.hashSync(password, salt);
+            resolve(hashPassword);
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let getAllUser = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let users = db.User.findAll({
+                raw: true,
+            });
+            resolve(users);
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+module.exports = {
+    createNewUser: createNewUser,
+    hashUserPassword, hashUserPassword,
+    getAllUser: getAllUser,
+}
